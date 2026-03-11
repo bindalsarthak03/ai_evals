c31800544b822f6a47791fe402981f1d25fa9cf1
Sarthak Bindal <bindalsarthak03@gmail.com>
2026-03-11 11:11:27 +0530

    questions pipeline and integrate Gemini API for answer generation and evaluation

---
+++ src/retreiver.py
@@ -0,0 +1,21 @@
+from embedder import Embedder
+from vector_store import VectorStore
+
+class Retriever:
+    
+    def __init__(self):
+        self.embedder = Embedder()
+        self.vector_store = VectorStore()
+        
+    def retreive_context(self,question,top_k=3):
+        query_embedding = self.embedder.create_embeddings([question])[0]
+        
+        results = self.vector_store.collection.query(
+            query_embeddings=[query_embedding.tolist()],
+            n_results=top_k
+        )
+        
+        documents = results["documents"][0]
+        metadatas = results["metadatas"][0]
+        
+        return documents,metadatas
